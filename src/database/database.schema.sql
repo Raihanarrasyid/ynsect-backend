0203d0fc0bc253c7ecc4f40644c1ef85190b77dc
Raihanarrasyid <raihanarrasyid1704@gmail.com>
2024-03-12 12:31:58 +0700

    feat : add sql schema

---
+++ src/database/database.schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE
+    Users (
+        id VARCHAR(255) PRIMARY KEY,
+        email VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        no_telp VARCHAR(20),
+        alamat TEXT
+    );
