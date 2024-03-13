c0072b41a13e015be0b78b0dbe40611fdcd66100
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-03-13 23:10:10 +0700

    build: setup express js server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const UserRouter = require("./routes/route.user");
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use("/api", UserRouter);
+
+app.listen(port, () => {
+  console.log("Server aktif");
+  console.log(`http://localhost:${port}`);
+});
