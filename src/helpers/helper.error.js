2625317e827a6482f81bddc187cce60e1ee9c07d
Raihanarrasyid <raihanarrasyid1704@gmail.com>
2024-03-18 19:55:52 +0700

    feat : specify error helper

---
+++ src/helpers/helper.error.js
@@ -0,0 +1,21 @@
+class ErrorHelper {
+  static NotFoundError = (req, res, message) => {
+    // Implement here
+  };
+
+  static InternalServerError = (req, res, message) => {
+    // Implement here
+  };
+
+  static BadRequestError = (req, res, message) => {
+    // Implement here
+  };
+
+  static UnauthorizedError = (req, res, message) => {
+    // Implement here
+  };
+
+  static ForbiddenError = (req, res, message) => {
+    // Implement here
+  };
+}
