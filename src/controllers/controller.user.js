71dc6da99aff29f24186a339254c5039970c93be
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-03-21 00:57:09 +0700

    feat: created user endpoint

--- src/controllers/controller.user.js
+++ src/controllers/controller.user.js
@@ -1,23 +1,58 @@
-class UserController {
-  static getUsers(req, res) {
-    const arrayOfData = [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jhon@gmail.com'
-      },
-      {
-        id: 2,
-        name: 'Jane Doe',
-        email: 'jane@gmail.com'
-      }
-    ];
+const UserModel = require('../models/model.user');
+const user = new UserModel();
 
-    return res.status(200).json({
-      code: 200,
-      status: 'success',
-      data: arrayOfData
-    });
+class UserController {
+  static async getUsers(req, res) {
+    try {
+      const results = await user.getAll();
+      return res.status(200).json({
+        code: 200,
+        status: 'success',
+        data: results
+      });
+    } catch (error) {
+      return res.status(500).json({
+        code: 500,
+        status: 'error',
+        message: error.message
+      });
+    }
+  }
+
+  static async getUserById(req, res) {
+    try {
+      const userId = parseInt(req.params.userId);
+      const results = await user.getById(userId);
+      return res.status(200).json({
+        code: 200,
+        status: 'success',
+        data: results
+      });
+    } catch (error) {
+      return res.status(500).json({
+        code: 500,
+        status: 'error',
+        message: error.message
+      });
+    }
+  }
+
+  static async createUser(req, res) {
+    try {
+      const data = req.body;
+      const results = await user.create(data);
+      return res.status(201).json({
+        code: 201,
+        status: 'success',
+        data: results
+      });
+    } catch (error) {
+      return res.status(500).json({
+        code: 500,
+        status: 'error',
+        message: error.message
+      });
+    }
   }
 }
 
--- src/routes/route.user.js
+++ src/routes/route.user.js
@@ -3,5 +3,7 @@ const UserRouter = express.Router();
 const UserController = require('../controllers/controller.user');
 
 UserRouter.get('/users', UserController.getUsers);
+UserRouter.get('/users/:userId', UserController.getUserById);
+UserRouter.post('/users/signup', UserController.createUser);
 
 module.exports = UserRouter;
