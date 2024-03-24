825945d07fceb0c945939de4e6c3de1d7ce646fa
Raihanarrasyid <raihanarrasyid1704@gmail.com>
2024-03-24 11:52:51 +0700

    feat : create forum model

---
+++ src/models/model.forum.js
@@ -0,0 +1,25 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+class ForumModel {
+  static async getAllForum() {
+    return await prisma.forum.findMany();
+  }
+
+  static async createForum(data) {
+    return await prisma.forum.create({
+      data: {
+        content: data.question,
+        userId: data.userId
+      }
+    });
+  }
+
+  static async getForumById(id) {
+    return await prisma.forum.findUnique({
+      where: {
+        id: parseInt(id)
+      }
+    });
+  }
+}
