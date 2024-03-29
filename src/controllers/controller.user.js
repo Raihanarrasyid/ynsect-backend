1258ca81162b4a7551ba5e861eb759ed1daa20e0
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-03-29 23:48:32 +0700

    feat: jwt integration for middleware/authorization

--- src/controllers/controller.user.js
+++ src/controllers/controller.user.js
@@ -1,18 +1,15 @@
 const UserModel = require('../models/model.user');
 const user = new UserModel();
-const responseHelper = require('../helpers/helper.response');
+const response = require('../helpers/helper.error');
+const { generateToken } = require('../helpers/helper.authorization');
 
 class UserController {
   static async getUsers(req, res) {
     try {
       const results = await user.getAll();
-      return responseHelper(res, 200, 'success', results);
+      return response.DataFound(req, res, 'Data ditemukan!', results);
     } catch (error) {
-      return res.status(500).json({
-        code: 500,
-        status: 'error',
-        message: error.message
-      });
+      return response.InternalServerError(req, res, error.message);
     }
   }
 
@@ -20,13 +17,9 @@ class UserController {
     try {
       const userId = parseInt(req.params.userId);
       const results = await user.getById(userId);
-      return responseHelper(res, 200, 'success', results);
+      return response.DataFound(req, res, 'Data ditemukan!', results);
     } catch (error) {
-      return res.status(500).json({
-        code: 500,
-        status: 'error',
-        message: error.message
-      });
+      return response.InternalServerError(req, res, error.message);
     }
   }
 
@@ -34,13 +27,11 @@ class UserController {
     try {
       const data = req.body;
       const results = await user.create(data);
-      return responseHelper(res, 201, 'success', results);
+      const token = generateToken({ id: results.insertId });
+      results.token = token;
+      return response.DataCreated(req, res, 'Berhasil mendaftar!', results);
     } catch (error) {
-      return res.status(500).json({
-        code: 500,
-        status: 'error',
-        message: error.message
-      });
+      return response.InternalServerError(req, res, error.message);
     }
   }
 }
