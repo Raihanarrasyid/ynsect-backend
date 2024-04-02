0f4286534de028b9304b80080a4654f229fc683d
Raihanarrasyid <raihanarrasyid1704@gmail.com>
2024-04-02 17:53:29 +0700

    fix : match routes with docs

--- src/routes/route.like.js
+++ src/routes/route.like.js
@@ -2,7 +2,7 @@ const express = require('express');
 const likeRouter = express.Router();
 const likeController = require('../controllers/controller.like');
 
-likeRouter.post('/like', likeController.likeToggle);
-likeRouter.get('/like/:forumId', likeController.getLikeByForumId);
+likeRouter.post('/likes', likeController.likeToggle);
+likeRouter.get('/likes/:forumId', likeController.getLikeByForumId);
 
 module.exports = likeRouter;
