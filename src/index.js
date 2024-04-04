bb511169adcb1db771a42cd632b5acc5e17dca3e
Raihanarrasyid <raihanarrasyid1704@gmail.com>
2024-04-04 21:58:38 +0700

    fix : port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const UserRouter = require('./routes/route.user');
 const CartRouter = require('./routes/route.cart');
