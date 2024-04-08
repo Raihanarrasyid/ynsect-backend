69a1d1939436ab4c4b876fa4aea6b03f6b0d413c
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-04-08 21:39:56 +0700

    fix: remove authcheck

--- src/routes/route.forum.js
+++ src/routes/route.forum.js
@@ -3,8 +3,8 @@ const ForumRouter = express.Router();
 const ForumController = require('../controllers/controller.forum');
 const { authCheck } = require('../helpers/helper.authorization');
 
-ForumRouter.get('/forums', authCheck, ForumController.getAll);
+ForumRouter.get('/forums', ForumController.getAll);
 ForumRouter.post('/forums', authCheck, ForumController.create);
-ForumRouter.get('/forums/:forumId', authCheck, ForumController.getById);
+ForumRouter.get('/forums/:forumId', ForumController.getById);
 
 module.exports = ForumRouter;
