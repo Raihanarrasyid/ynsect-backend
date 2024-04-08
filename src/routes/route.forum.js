a1fdb5061284e5727fd99702e43c0ec7b9131d9f
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-04-08 16:03:48 +0700

    fix: routes name

--- src/routes/route.forum.js
+++ src/routes/route.forum.js
@@ -4,7 +4,7 @@ const ForumController = require('../controllers/controller.forum');
 const { authCheck } = require('../helpers/helper.authorization');
 
 ForumRouter.get('/forums', authCheck, ForumController.getAll);
-ForumRouter.post('/forum', authCheck, ForumController.create);
-ForumRouter.get('/forum/:forumId', authCheck, ForumController.getById);
+ForumRouter.post('/forums', authCheck, ForumController.create);
+ForumRouter.get('/forums/:forumId', authCheck, ForumController.getById);
 
 module.exports = ForumRouter;
--- src/routes/route.user.js
+++ src/routes/route.user.js
@@ -4,9 +4,9 @@ const UserController = require('../controllers/controller.user');
 const { authCheck } = require('../helpers/helper.authorization');
 
 UserRouter.get('/users', authCheck, UserController.getAll);
-UserRouter.get('/user/:userId', authCheck, UserController.getById);
-UserRouter.get('/user/:userEmail', authCheck, UserController.getByEmail);
-UserRouter.post('/user/register', UserController.register);
-UserRouter.post('/user/login', UserController.login);
+UserRouter.get('/users/:userId', authCheck, UserController.getById);
+UserRouter.get('/users/:userEmail', authCheck, UserController.getByEmail);
+UserRouter.post('/users/register', UserController.register);
+UserRouter.post('/users/login', UserController.login);
 
 module.exports = UserRouter;
